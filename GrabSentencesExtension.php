a767436f90afdbeb6343c7e18850bfafc444353a
artgris <a.gribet@gmail.com>
2016-11-11 16:25:47 +0100

    Add files via upload

---
+++ GrabSentencesExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Twig;
+
+
+class GrabSentencesExtension extends \Twig_Extension
+{
+
+	public function getFilters()
+	{
+		return array(
+			new \Twig_SimpleFilter('grab_sentences', [$this, 'grabSentencesFilter']),
+		);
+	}
+
+	public function grabSentencesFilter($text, $size = 1)
+	{
+		preg_match_all("/[A-Z].*?[\.!?](?:\s|$)/", $text, $matches);
+		return implode('', array_slice($matches[0], 0, $size));
+
+	}
+
+	public function getName()
+	{
+		return 'grab_sentences_extension';
+	}
+
+
+}
